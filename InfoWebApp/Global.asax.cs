da5f732ddc4657b4d0dbbb91b044d0c04b0bde11
acaktas <acaktas@hattrick.hr>
2018-03-07 11:17:12 +0100

    initial commit

---
+++ InfoWebApp/Global.asax.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Caching;
+using InfoWebApp.Scheduler;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using InfoWebApp.Scraper;
+
+namespace InfoWebApp
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            JobScheduler.Schedule();
+
+            var cash = new Cache();
+            cash.Insert("articles", new List<Article>(), null, Cache.NoAbsoluteExpiration, TimeSpan.FromMinutes(1));
+        }
+    }
+}
---
+++ InfoWebApp/Scheduler/JobScheduler.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Specialized;
+using System.Threading.Tasks;
+using Quartz;
+using Quartz.Impl;
+
+namespace InfoWebApp.Scheduler
+{
+    public class JobScheduler
+    {
+        public static void Schedule()
+        {
+            // construct a scheduler factory
+            var props = new NameValueCollection
+            {
+                { "quartz.serializer.type", "binary" }
+            };
+            var factory = new StdSchedulerFactory(props);
+
+            // get a scheduler
+            var scheduler = factory.GetScheduler().Result;
+            scheduler.Start().Wait();
+
+            //IScheduler scheduler = StdSchedulerFactory.GetDefaultScheduler();
+            //scheduler.Start();
+
+            var job = JobBuilder.Create<ScraperJob>().Build();
+
+            var trigger = TriggerBuilder.Create()
+                .WithIdentity("trigger1", "group1")
+                .StartNow()
+                .WithSimpleSchedule(s => s
+                    .WithIntervalInMinutes(1)
+                    .RepeatForever()
+                )
+                .Build();
+
+            scheduler.ScheduleJob(job, trigger).Wait();
+        }
+    }
+}
